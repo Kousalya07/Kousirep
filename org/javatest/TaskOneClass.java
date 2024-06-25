6503d5b273fdb13bc73ceb0160ae83190356882e
Kousalya07 <kousalyacse.12@gmail.com>
2024-06-25 23:23:21 +0800

    Code added

---
+++ org/javatest/TaskOneClass.java
@@ -0,0 +1,17 @@
+package org.javatest;
+
+public class TaskOneClass {
+      void add(int a,int b)
+      {
+    	  int c;
+    	  c=a+b;
+    	  System.out.println(c);
+      }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        TaskOneClass toc =new TaskOneClass();
+        toc.add(10,42);
+
+	}
+
+}
